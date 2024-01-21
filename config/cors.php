d6ab4981944c8c8f0fe34f7337880e063a9322bf
Tobiasz Tonn <toneq@tlen.pl>
2024-01-21 20:28:15 +0100

    zmiana configu dla cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => ['http://localhost:4200', 'https://skins-hut.com', 'https://login.skins-hut.com'],
 
     'allowed_origins_patterns' => [],
 
